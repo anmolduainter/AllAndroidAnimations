f6fdf36565d57b4f91cbf3d123b8daf5eaf3ff74
anmolduainter <anmolduainter@gmail.com>
2017-05-14 00:47:09 +0530

    .
    .

---
+++ app/src/main/java/com/example/joginderpal/allandroidanimations/Arc/arc_main_two.java
@@ -0,0 +1,34 @@
+package com.example.joginderpal.allandroidanimations.Arc;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.joginderpal.allandroidanimations.R;
+
+/**
+ * Created by joginderpal on 14-05-2017.
+ */
+public class arc_main_two extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.arc_main_two);
+        fab= (FloatingActionButton) findViewById(R.id.fab_arc_main_two);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+
+            }
+        });
+
+    }
+}
